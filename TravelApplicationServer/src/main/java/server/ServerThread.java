e8a7a2a8185225064485c31c164febaee749c08e
fahricansanli <32355424+fahricansanli@users.noreply.github.com>
2019-12-06 23:24:00 +0300

    Add files via upload

--- TravelApplicationServer/src/main/java/server/ServerThread.java
+++ TravelApplicationServer/src/main/java/server/ServerThread.java
@@ -41,7 +41,81 @@ public class ServerThread extends Thread {
     }
 
     public void executeMessages() throws IOException {
-        String input;
+
+        String input = inputFromClient.readLine();
+        try {
+            inputMessage = GsonBuilderHelper.getJsonBuilder().fromJson(input, Message.class);
+            switch (inputMessage.getType()) {
+                case GET_DISTRICT:
+                    ArrayList<District> districts = Database.getDistricts();
+                    Message outputMessage = new Message(Type.GET_DISTRICT, WriteReadOperation.convertToJSON(districts), "", "", "");
+                    outputForClient.writeBytes(WriteReadOperation.convertToJSON(outputMessage) + '\n');
+                    break;
+
+                case GET_LEISURE_ACTIVITY:
+                    ArrayList<LeisureTimeActivity> leisureTimeActivities = Database.getLeisureTimeActivities();
+                    Message outputMessage2 = new Message(Type.GET_LEISURE_ACTIVITY, WriteReadOperation.convertToJSON(leisureTimeActivities), "", "", "");
+                    outputForClient.writeBytes(WriteReadOperation.convertToJSON(outputMessage2) + '\n');
+                    break;
+
+                case LOGIN:
+                    User loginUser = WriteReadOperation.convertToObject(inputMessage.getJsonData(), User.class);
+                    boolean isUserAuthenticated = Database.authenticateUser(loginUser);
+                    Message outputMessageForAuthentication = new Message(Type.LOGIN, WriteReadOperation.convertToJSON(isUserAuthenticated), "", "", "");
+                    outputForClient.writeBytes(WriteReadOperation.convertToJSON(outputMessageForAuthentication) + '\n');
+                    break;
+                case REGISTER:
+                    User userToSave = WriteReadOperation.convertToObject(inputMessage.getJsonData(), User.class);
+                    Database.saveUser(userToSave);
+                    Message outputMessageForSave = new Message(Type.REGISTER, WriteReadOperation.convertToJSON(true), "", "", "");
+                    outputForClient.writeBytes(WriteReadOperation.convertToJSON(outputMessageForSave) + '\n');
+                    break;
+                case GET_PLACES:
+                    District selectedDistrict = WriteReadOperation.convertToObject(inputMessage.getJsonData(), District.class);
+                    LeisureTimeActivity selectedActivity = WriteReadOperation.convertToObject(inputMessage.getOptionalSecondData(), LeisureTimeActivity.class);
+                    ArrayList<Place> places = Database.getPlaces(selectedDistrict, selectedActivity);
+                    Message outputMessageForGetPlaces = new Message(Type.GET_PLACES, WriteReadOperation.convertToJSON(places), "", "", "");
+                    outputForClient.writeBytes(WriteReadOperation.convertToJSON(outputMessageForGetPlaces) + '\n');
+                    break;
+                case UPDATE_COMMENT_LIST:
+                    District district = WriteReadOperation.convertToObject(inputMessage.getJsonData(), District.class);
+                    LeisureTimeActivity leisureTimeActivity = WriteReadOperation.convertToObject(inputMessage.getOptionalSecondData(), LeisureTimeActivity.class);
+                    Place place = WriteReadOperation.convertToObject(inputMessage.getOptionalThirdData(), Place.class);
+                    Comment comment = WriteReadOperation.convertToObject(inputMessage.getOptionalFourthData(), Comment.class);
+                    ArrayList<Comment> modifiedComments = Database.addComment(district, leisureTimeActivity, place, comment);
+                    Message outputMessageForAddComment = new Message(Type.UPDATE_COMMENT_LIST, WriteReadOperation.convertToJSON(modifiedComments), "", "", "");
+                    outputForClient.writeBytes(WriteReadOperation.convertToJSON(outputMessageForAddComment) + '\n');
+                    //shouldWorkInfinitely = true;
+                    //updateAllThreads(outputMessageForAddComment);
+                    break;
+                case PLACE_DETAIL:
+                    District district2 = WriteReadOperation.convertToObject(inputMessage.getJsonData(), District.class);
+                    LeisureTimeActivity leisureTimeActivity2 = WriteReadOperation.convertToObject(inputMessage.getOptionalSecondData(), LeisureTimeActivity.class);
+                    Place place2 = WriteReadOperation.convertToObject(inputMessage.getOptionalThirdData(), Place.class);
+                    ArrayList<Comment> comments = Database.getComments(district2, leisureTimeActivity2, place2);
+                    Message outputMessageForPlaceDetail = new Message(Type.PLACE_DETAIL, WriteReadOperation.convertToJSON(comments), "", "", "");
+                    outputForClient.writeBytes(WriteReadOperation.convertToJSON(outputMessageForPlaceDetail) + '\n');
+                    break;
+                        /*case NOTIFY_SERVER_TO_LISTEN:
+                            District districtToListen = WriteReadOperation.convertToObject(inputMessage.getJsonData(), District.class);
+                            LeisureTimeActivity leisureTimeActivityToListen  = WriteReadOperation.convertToObject(inputMessage.getOptionalSecondData(), LeisureTimeActivity.class);
+                            Place placeToListen  = WriteReadOperation.convertToObject(inputMessage.getOptionalThirdData(), Place.class);
+                            placeToAddComment = Constants.COMMENT_PATH.replace("[district]", districtToListen.getDistrictName().toLowerCase())
+                                    .replace("[leisure_time_activity]", leisureTimeActivityToListen.getName().toLowerCase())
+                                    .replace("[placeID]", placeToListen.getID() + "");
+                            shouldWorkInfinitely = true;
+                            break;
+                        case STOP_LISTENING_SERVER:
+                            shouldWorkInfinitely = false;
+                            break;*/
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+        /*String input;
         while (true) {
             input = inputFromClient.readLine();
             if (input != null) {
@@ -98,7 +172,7 @@ public class ServerThread extends Thread {
                             Message outputMessageForPlaceDetail = new Message(Type.PLACE_DETAIL, WriteReadOperation.convertToJSON(comments), "", "", "");
                             outputForClient.writeBytes(WriteReadOperation.convertToJSON(outputMessageForPlaceDetail) + '\n');
                             break;
-                        /*case NOTIFY_SERVER_TO_LISTEN:
+                        case NOTIFY_SERVER_TO_LISTEN:
                             District districtToListen = WriteReadOperation.convertToObject(inputMessage.getJsonData(), District.class);
                             LeisureTimeActivity leisureTimeActivityToListen  = WriteReadOperation.convertToObject(inputMessage.getOptionalSecondData(), LeisureTimeActivity.class);
                             Place placeToListen  = WriteReadOperation.convertToObject(inputMessage.getOptionalThirdData(), Place.class);
@@ -109,7 +183,7 @@ public class ServerThread extends Thread {
                             break;
                         case STOP_LISTENING_SERVER:
                             shouldWorkInfinitely = false;
-                            break;*/
+                            break;
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -119,9 +193,9 @@ public class ServerThread extends Thread {
             if(!shouldWorkInfinitely){
                 break;
             }
-        }
+        }*/
 
-    }
+}
 
     /*public void updateAllThreads(Message message) throws IOException {
         for (ServerThread thread : ApplicationServer.serverThreads) {
@@ -131,4 +205,4 @@ public class ServerThread extends Thread {
             }
         }
     }*/
-}
+
